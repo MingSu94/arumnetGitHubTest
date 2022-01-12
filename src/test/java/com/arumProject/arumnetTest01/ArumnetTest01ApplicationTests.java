290c614163390f28dc9b3150721a86f72901cec5
MingSu94 <dhrwjdch97@gmail.com>
2022-01-12 19:05:11 +0900

    두번째 올려봅니다.

--- src/test/java/com/arumProject/arumnetTest01/ArumnetTest01ApplicationTests.java
+++ src/test/java/com/arumProject/arumnetTest01/ArumnetTest01ApplicationTests.java
@@ -11,6 +11,7 @@ class ArumnetTest01ApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
---
+++ src/test/java/com/arumProject/arumnetTest01/Test01.java
@@ -0,0 +1,8 @@
+package com.arumProject.arumnetTest01;
+
+public class Test01 {
+    public static void main(String []args) {
+        System.out.println("안녕하세요?");
+    }
+
+}
