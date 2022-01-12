4e1e44d7ede26f4a73e461e3d350efe5fe5e8e67
MingSu94 <dhrwjdch97@gmail.com>
2022-01-12 18:52:55 +0900

    처음 올려봅니다... 소리내어 외칩니다!

---
+++ src/test/java/com/arumProject/arumnetTest01/ArumnetTest01ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.arumProject.arumnetTest01;
+
+import org.hibernate.loader.entity.NaturalIdEntityJoinWalker;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
+
+@SpringBootTest
+class ArumnetTest01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
